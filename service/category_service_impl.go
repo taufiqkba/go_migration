6b563c87f057cffdba639ea16296248a6afcfbbd
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-08 11:35:41 +0700

    Implement Category Service

---
+++ service/category_service_impl.go
@@ -0,0 +1,81 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"go_restful_api/helper"
+	"go_restful_api/model/domain"
+	"go_restful_api/model/web"
+	"go_restful_api/repository"
+)
+
+type CategoryServiceImpl struct {
+	CategoryRepository repository.CategoryRepository
+	DB                 *sql.DB
+}
+
+func (service CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category := domain.Category{
+		Name: request.Name,
+	}
+	category = service.CategoryRepository.Save(ctx, tx, category)
+	return helper.ToCategoryResponse(category)
+
+}
+
+func (service CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	//to check category available or not
+	category, err := service.CategoryRepository.FindById(ctx, tx, request.Id)
+	helper.PanicIfError(err) // if category not found send error
+
+	// if category available send request
+	category.Name = request.Name
+
+	category = service.CategoryRepository.Update(ctx, tx, category)
+	return helper.ToCategoryResponse(category)
+}
+
+func (service CategoryServiceImpl) Delete(ctx context.Context, categoryId int) {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	//check category id available or not
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err) // if category id not found send error
+
+	// if category id available delete it
+	service.CategoryRepository.Delete(ctx, tx, category)
+}
+
+func (service CategoryServiceImpl) FindById(ctx context.Context, categoryId int) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	//check category id available or not
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err) // if category id not found send error
+
+	// if category id available return it
+	return helper.ToCategoryResponse(category)
+}
+
+func (service CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	categories := service.CategoryRepository.FindAll(ctx, tx)
+
+	//using model helper ToCategoryResponses to conversion
+	return helper.ToCategoryResponses(categories)
+}
