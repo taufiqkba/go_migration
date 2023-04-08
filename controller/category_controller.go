135d719c37c9fdffeef4865c08a74b2358877e96
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-08 14:31:01 +0700

    Implement Category Controller

--- controller/category_controller.go
+++ controller/category_controller.go
@@ -7,9 +7,9 @@ import (
 
 // CategoryController contract
 type CategoryController interface {
-	Create(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	Update(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	Delete(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	FindById(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	FindAll(w http.ResponseWriter, r http.Request, params httprouter.Params)
+	Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
 }
---
+++ controller/category_controller_impl.go
@@ -0,0 +1,89 @@
+package controller
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"go_restful_api/helper"
+	"go_restful_api/model/web"
+	"go_restful_api/service"
+	"net/http"
+	"strconv"
+)
+
+type CategoryControllerImpl struct {
+	CategoryService service.CategoryService
+}
+
+func (controller *CategoryControllerImpl) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//Read from request body
+	categoryCreateRequest := web.CategoryCreateRequest{}
+	helper.ReadFromRequestBody(request, &categoryCreateRequest)
+
+	//Response
+	categoryResponse := controller.CategoryService.Create(request.Context(), categoryCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   categoryResponse,
+	}
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//Body Request
+	categoryUpdateRequest := web.CategoryUpdateRequest{}
+	helper.ReadFromRequestBody(request, &categoryUpdateRequest)
+
+	//using parameter id
+	categoryId := params.ByName("categoryId")
+	id, err := strconv.Atoi(categoryId) // conversion to int type
+	helper.PanicIfError(err)
+
+	categoryUpdateRequest.Id = id
+
+	//response
+	categoryResponse := controller.CategoryService.Update(request.Context(), categoryUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   categoryResponse,
+	}
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//	select id
+	categoryId := params.ByName("categoryId")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	controller.CategoryService.Delete(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+	}
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	categoryId := params.ByName("categoryId")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	categoryResponse := controller.CategoryService.FindById(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   categoryResponse,
+	}
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	categoryResponses := controller.CategoryService.FindAll(request.Context())
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   categoryResponses,
+	}
+	helper.WriteToResponseBody(writer, webResponse)
+}
