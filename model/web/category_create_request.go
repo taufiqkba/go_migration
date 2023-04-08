039a1930a943e8e9854132d1b00d1aa2530eff1d
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-08 11:44:53 +0700

    Implement validation category

--- model/web/category_create_request.go
+++ model/web/category_create_request.go
@@ -1,5 +1,5 @@
 package web
 
 type CategoryCreateRequest struct {
-	Name string
+	Name string `validate:"required,unique,max=200,min=1"`
 }
--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"database/sql"
+	"github.com/go-playground/validator/v10"
 	"go_restful_api/helper"
 	"go_restful_api/model/domain"
 	"go_restful_api/model/web"
@@ -12,9 +13,14 @@ import (
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+	Validate           *validator.Validate
 }
 
 func (service CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	//validation first
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
@@ -28,6 +34,10 @@ func (service CategoryServiceImpl) Create(ctx context.Context, request web.Categ
 }
 
 func (service CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	//validation first
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
