c9834e0e4d1a3ded9bf8a21805ef09a4dac7a79e
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-11 22:08:46 +0700

    Create integration test controller

---
+++ test/category_controller_test.go
@@ -0,0 +1,264 @@
+package test
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+	"go_restful_api/app"
+	"go_restful_api/controller"
+	"go_restful_api/helper"
+	"go_restful_api/middleware"
+	"go_restful_api/model/domain"
+	"go_restful_api/repository"
+	"go_restful_api/service"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+// Connection database test
+func setupTestDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go_restful_api_test")
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
+
+// setup router test
+func setupRouter(db *sql.DB) http.Handler {
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+	router := app.NewRouter(categoryController)
+
+	return middleware.NewAuthMiddleware(router)
+}
+
+func truncateCategory(db *sql.DB) {
+	db.Exec("TRUNCATE category")
+}
+
+func TestCreateCategorySuccess(t *testing.T) {
+	db := setupTestDB()
+	truncateCategory(db)
+	router := setupRouter(db)
+
+	requestBody := strings.NewReader(`{"name":"Gadget"}`)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/api/categories", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
+	assert.Equal(t, "Gadget", responseBody["data"].(map[string]interface{})["name"])
+}
+
+func TestCreateCategoryFailed(t *testing.T) {
+	db := setupTestDB()
+	truncateCategory(db)
+	router := setupRouter(db)
+
+	requestBody := strings.NewReader(`{"name":""}`)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/api/categories", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 400, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+
+	json.Unmarshal(body, &responseBody)
+	assert.Equal(t, 400, int(responseBody["code"].(float64)))
+	assert.Equal(t, "BAD REQUEST", responseBody["status"])
+}
+
+func TestUpdateCategorySuccess(t *testing.T) {
+	//setup db
+	db := setupTestDB()
+	truncateCategory(db) // delete all data before running test
+
+	// insert into database
+	tx, _ := db.Begin()
+	categoryRepository := repository.NewCategoryRepository()
+	category := categoryRepository.Save(context.Background(), tx, domain.Category{
+		Name: "Gadget",
+	})
+	tx.Commit()
+
+	router := setupRouter(db)
+
+	requestBody := strings.NewReader(`{"name":"Gadget"}`)
+	request := httptest.NewRequest(http.MethodPut, "http://localhost:3000/api/categories/"+strconv.Itoa(category.Id), requestBody)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	// test
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
+	assert.Equal(t, category.Id, int(responseBody["data"].(map[string]interface{})["id"].(float64)))
+	assert.Equal(t, "Gadget", responseBody["data"].(map[string]interface{})["name"])
+}
+
+func TestUpdateCategoryFailed(t *testing.T) {
+	//setup db
+	db := setupTestDB()
+	truncateCategory(db) // delete all data before running test
+
+	// insert to database
+	tx, _ := db.Begin()
+	categoryRepository := repository.NewCategoryRepository()
+	category := categoryRepository.Save(context.Background(), tx, domain.Category{
+		Name: "Gadget",
+	})
+	tx.Commit()
+
+	//setup router
+	router := setupRouter(db)
+
+	//setup request body
+	requestBody := strings.NewReader(`{"name":""}`)
+	request := httptest.NewRequest(http.MethodPut, "http://localhost:3000/api/categories/"+strconv.Itoa(category.Id), requestBody)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	//setup response
+	response := recorder.Result()
+	assert.Equal(t, 400, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	// test
+	assert.Equal(t, 400, int(responseBody["code"].(float64)))
+	assert.Equal(t, "BAD REQUEST", responseBody["status"])
+}
+
+func TestGetCategorySuccess(t *testing.T) {
+	//setup db
+	db := setupTestDB()
+	truncateCategory(db) // delete all data before running test
+
+	// insert to database
+	tx, _ := db.Begin()
+	categoryRepository := repository.NewCategoryRepository()
+	category := categoryRepository.Save(context.Background(), tx, domain.Category{
+		Name: "Gadget",
+	})
+	tx.Commit()
+
+	//setup router
+	router := setupRouter(db)
+
+	//setup request body
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/categories/"+strconv.Itoa(category.Id), nil)
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	//setup response
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	// test
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
+	assert.Equal(t, category.Id, int(responseBody["data"].(map[string]interface{})["id"].(float64)))
+	assert.Equal(t, category.Name, responseBody["data"].(map[string]interface{})["name"])
+}
+
+func TestGetCategoryFailed(t *testing.T) {
+	//setup db
+	db := setupTestDB()
+	truncateCategory(db) // delete all data before running test
+
+	//setup router
+	router := setupRouter(db)
+
+	//setup request body
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/categories/404", nil)
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	//setup response
+	response := recorder.Result()
+	assert.Equal(t, 404, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	// test
+	assert.Equal(t, 404, int(responseBody["code"].(float64)))
+	assert.Equal(t, "NOT FOUND", responseBody["status"])
+}
+
+func TestDeleteCategorySuccess(t *testing.T) {
+
+}
+
+func TestDeleteCategoryFailed(t *testing.T) {
+
+}
+
+func TestListCategorySuccess(t *testing.T) {
+
+}
+
+func TestUnauthorized(t *testing.T) {
+
+}
