1a1d2ca266ce2027e2e222e39c35c782deee5acf
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-11 22:23:38 +0700

    Refactor main function

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 	"go_restful_api/app"
 	"go_restful_api/controller"
-	"go_restful_api/exception"
 	"go_restful_api/helper"
 	"go_restful_api/middleware"
 	"go_restful_api/repository"
@@ -21,15 +19,7 @@ func main() {
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
-
-	router := httprouter.New()
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
