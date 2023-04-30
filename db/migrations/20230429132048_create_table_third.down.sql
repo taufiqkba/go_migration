097dbb5f3a509862e1d892574e3aa5d6cad6be3f
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-30 19:51:55 +0700

    Dirty State Migration

--- db/migrations/20230429132048_create_table_third.down.sql
+++ db/migrations/20230429132048_create_table_third.down.sql
@@ -1,6 +1,10 @@
 DROP TABLE IF EXISTS third;
 
+-- // Command Go Migration// --
 -- migrate -database "mysql://root:root@tcp(localhost:3306)/go_migration" -path db/migrations up
 -- migrate -database "mysql://root:root@tcp(localhost:3306)/go_migration" -path db/migrations down
 -- migrate -database "mysql://root:root@tcp(localhost:3306)/go_migration" -path db/migrations up 2 --> for migrate 2 level
 -- migrate -database "mysql://root:root@tcp(localhost:3306)/go_migration" -path db/migrations down 1 --> for rollback 1 level
+-- migrate create -ext sql -dir db/migrations sample_dirty_state
+-- migrate -database "mysql://root:root@tcp(localhost:3306)/go_migration" -path db/migrations version
+-- migrate -database "mysql://root:root@tcp(localhost:3306)/go_migration" -path db/migrations force 20230429132048
